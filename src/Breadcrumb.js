2aaaefcfa3d81ca1af6fca72029b5f73fc80a054
macroing <joergen.lundgren@macroing.org>
2024-07-15 08:47:42 +0200

    Update the Breadcrumb component.

--- src/Breadcrumb.js
+++ src/Breadcrumb.js
@@ -1,6 +1,5 @@
 "use client";
 
-import Link from "next/link";
 import { useState } from "react";
 
 import importedStyles from "./Breadcrumb.module.css";
@@ -17,12 +16,12 @@ export default function Breadcrumb(props) {
         {items.map((item, itemIndex) => (
           <li className={styles.item} key={"item-" + itemIndex}>
             {itemIndex > 0 && <span>/</span>}
-            {item.href ? <Link href={item.href}>{item.text}</Link> : <span>{item.text}</span>}
+            {item.href ? <a href={item.href}>{item.text}</a> : <span>{item.text}</span>}
           </li>
         ))}
       </ul>
       <ul className={styles.breadcrumb_mobile}>
-        {items.length > 0 && <li className={styles.item}>{items[0].href ? <Link href={items[0].href}>{items[0].text}</Link> : <span>{items[0].text}</span>}</li>}
+        {items.length > 0 && <li className={styles.item}>{items[0].href ? <a href={items[0].href}>{items[0].text}</a> : <span>{items[0].text}</span>}</li>}
         {items.length > 3 && (
           <li className={styles.item + " " + styles.item_button} onClick={(e) => setIsMenuVisible(!isMenuVisible)}>
             <span aria-hidden className="fa fa-ellipsis-h"></span>
@@ -31,14 +30,14 @@ export default function Breadcrumb(props) {
                 .filter((item, itemIndex) => itemIndex > 0 && itemIndex < items.length - 2)
                 .map((item, itemIndex) => (
                   <li className={styles.item} key={"item-" + itemIndex}>
-                    {item.href ? <Link href={item.href}>{item.text}</Link> : <span>{item.text}</span>}
+                    {item.href ? <a href={item.href}>{item.text}</a> : <span>{item.text}</span>}
                   </li>
                 ))}
             </ul>
           </li>
         )}
-        {items.length > 1 && <li className={styles.item}>{items[items.length - 2].href ? <Link href={items[items.length - 2].href}>{items[items.length - 2].text}</Link> : <span>{items[items.length - 2].text}</span>}</li>}
-        {items.length > 2 && <li className={styles.item}>{items[items.length - 1].href ? <Link href={items[items.length - 1].href}>{items[items.length - 1].text}</Link> : <span>{items[items.length - 1].text}</span>}</li>}
+        {items.length > 1 && <li className={styles.item}>{items[items.length - 2].href ? <a href={items[items.length - 2].href}>{items[items.length - 2].text}</a> : <span>{items[items.length - 2].text}</span>}</li>}
+        {items.length > 2 && <li className={styles.item}>{items[items.length - 1].href ? <a href={items[items.length - 1].href}>{items[items.length - 1].text}</a> : <span>{items[items.length - 1].text}</span>}</li>}
       </ul>
     </nav>
   );
