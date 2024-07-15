46d32ee92d67c731420b54a539bbf2f9cacf6b31
macroing <joergen.lundgren@macroing.org>
2024-07-15 19:01:57 +0200

    Update the Breadcrumb component.

--- src/Breadcrumb.js
+++ src/Breadcrumb.js
@@ -6,9 +6,14 @@ import importedStyles from "./Breadcrumb.module.css";
 
 export default function Breadcrumb(props) {
   const items = props.items;
+  const linkFactory = props.linkFactory || defaultLinkFactory;
   const styles = props.styles || importedStyles;
 
   const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+  function defaultLinkFactory(href, children) {
+    return <a href={href}>{children}</a>;
+  }
 
   return (
     <nav className={styles.breadcrumb_container}>
@@ -16,12 +21,12 @@ export default function Breadcrumb(props) {
         {items.map((item, itemIndex) => (
           <li className={styles.item} key={"item-" + itemIndex}>
             {itemIndex > 0 && <span>/</span>}
-            {item.href ? <a href={item.href}>{item.text}</a> : <span>{item.text}</span>}
+            {item.href ? linkFactory(item.href, item.text) : <span>{item.text}</span>}
           </li>
         ))}
       </ul>
       <ul className={styles.breadcrumb_mobile}>
-        {items.length > 0 && <li className={styles.item}>{items[0].href ? <a href={items[0].href}>{items[0].text}</a> : <span>{items[0].text}</span>}</li>}
+        {items.length > 0 && <li className={styles.item}>{items[0].href ? linkFactory(items[0].href, items[0].text) : <span>{items[0].text}</span>}</li>}
         {items.length > 3 && (
           <li className={styles.item + " " + styles.item_button} onClick={(e) => setIsMenuVisible(!isMenuVisible)}>
             <span aria-hidden className="fa fa-ellipsis-h"></span>
@@ -30,14 +35,14 @@ export default function Breadcrumb(props) {
                 .filter((item, itemIndex) => itemIndex > 0 && itemIndex < items.length - 2)
                 .map((item, itemIndex) => (
                   <li className={styles.item} key={"item-" + itemIndex}>
-                    {item.href ? <a href={item.href}>{item.text}</a> : <span>{item.text}</span>}
+                    {item.href ? linkFactory(item.href, item.text) : <span>{item.text}</span>}
                   </li>
                 ))}
             </ul>
           </li>
         )}
-        {items.length > 1 && <li className={styles.item}>{items[items.length - 2].href ? <a href={items[items.length - 2].href}>{items[items.length - 2].text}</a> : <span>{items[items.length - 2].text}</span>}</li>}
-        {items.length > 2 && <li className={styles.item}>{items[items.length - 1].href ? <a href={items[items.length - 1].href}>{items[items.length - 1].text}</a> : <span>{items[items.length - 1].text}</span>}</li>}
+        {items.length > 1 && <li className={styles.item}>{items[items.length - 2].href ? linkFactory(items[items.length - 2].href, items[items.length - 2].text) : <span>{items[items.length - 2].text}</span>}</li>}
+        {items.length > 2 && <li className={styles.item}>{items[items.length - 1].href ? linkFactory(items[items.length - 1].href, items[items.length - 1].text) : <span>{items[items.length - 1].text}</span>}</li>}
       </ul>
     </nav>
   );
